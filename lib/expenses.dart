7872f97c3f03a65538cb23411943274cbaab4cf0
Juti <jbuican19@gmail.com>
2025-01-14 10:00:33 +0000

    Update expenses.dart
    import expenses list widget that send list of expenses and build ListView (for that widget)

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_app/models/expense.dart';
+import 'package:expense_app/models/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,13 +12,29 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  // add dummy
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Food',
+        amount: 50,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Movie tickets',
+        amount: 30,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+  //
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Text('The chart'),
-          Text('Expenses list...'),
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          )
         ],
       ),
     );
