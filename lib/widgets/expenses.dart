3b10c082e0aca9ffbaa45406be5cb14c61cf85ce
Juti <jbuican19@gmail.com>
2025-01-18 09:49:32 +0000

    Update expenses.dart
    import the new expense form widget

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:expense_app/models/expense.dart';
 import 'package:expense_app/widgets/expense_list/expenses_list.dart';
-import 'package:flutter/material.dart';
+import 'package:expense_app/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -25,10 +26,20 @@ class _ExpensesState extends State<Expenses> {
         date: DateTime.now(),
         category: Category.leisure),
   ];
-  //
+
+   void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Personal Expense Tracker"),
+        actions: [
+          IconButton(onPressed: _openAddExpenseOverlay, icon: Icon(Icons.add))
+        ],
+      ),
       body: Column(
         children: [
           const Text('The chart'),
