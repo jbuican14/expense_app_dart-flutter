a6ffd15b1b5c5defc97888778ac2ea017849d4b2
Juti <jbuican19@gmail.com>
2025-01-25 09:23:22 +0000

    change formatter style to Month x, xyz in expense.dart model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart'; 
 import 'package:uuid/uuid.dart';
 
-final formatter = DateFormat.yMd();
+final formatter = DateFormat.yMMMd();
 
 const uuid = Uuid();
 
