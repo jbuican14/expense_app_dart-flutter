db43b57c13e1efefe666ded30772a2e0b0dc63d8
Juti <jbuican19@gmail.com>
2025-02-03 21:37:01 +0000

    Update new_expense.dart
    avoid element gets covered

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -81,7 +81,7 @@ class _NewExpensesState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsets.fromLTRB(16, 48, 16, 16),,
       child: Column(
         children: [
           TextField(
