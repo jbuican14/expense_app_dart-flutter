c13adfab2c3056f1a874c0c2132da6b5770966be
Juti <jbuican19@gmail.com>
2025-02-03 21:40:12 +0000

    Update expenses_list.dart
    add swipping left to remove item (we don't detect direction in this case but function will get 'direction' onDismissed so we can call on remove

--- lib/widgets/expense_list/expenses_list.dart
+++ lib/widgets/expense_list/expenses_list.dart
@@ -3,15 +3,20 @@ import 'package:expense_app/models/expense.dart';
 import 'package:expense_app/widgets/expense_list/expenses_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpensesItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => {onRemoveExpense(expenses[index])},
+        child: ExpensesItem(expenses[index]),
+      ),
     );
   }
 }
