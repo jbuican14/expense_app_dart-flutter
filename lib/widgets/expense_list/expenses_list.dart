ecb63042103154a6e13ea7539321df0918ea9738
Juti <jbuican19@gmail.com>
2025-02-10 14:21:24 +0000

    Update expenses_list.dart
    add list view

--- lib/widgets/expense_list/expenses_list.dart
+++ lib/widgets/expense_list/expenses_list.dart
@@ -3,7 +3,8 @@ import 'package:expense_app/models/expense.dart';
 import 'package:expense_app/widgets/expense_list/expenses_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses, required this.onRemoveExpense});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
   final void Function(Expense expense) onRemoveExpense;
@@ -14,6 +15,12 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+            color: Theme.of(context).colorScheme.error.withValues(alpha: 0.5),
+            // margin: EdgeInsets.symmetric(horizontal: 16)),
+            // margin: Theme.of(context).cardTheme,
+            margin: EdgeInsets.symmetric(
+                horizontal: Theme.of(context).cardTheme.margin!.horizontal)),
         onDismissed: (direction) => {onRemoveExpense(expenses[index])},
         child: ExpensesItem(expenses[index]),
       ),
