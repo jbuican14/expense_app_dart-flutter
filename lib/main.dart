e88b11c2297941b835ee094897c689e29542908c
Juti <jbuican19@gmail.com>
2025-04-13 19:13:04 +0100

    test meals screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,35 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-import 'package:expense_app/widgets/expenses.dart';
+import 'package:flutter_meal_app/data/dummy_data.dart';
+import 'package:flutter_meal_app/screens/meals.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:flutter_meal_app/screens/categories.dart';
 
-var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 69, 144, 229),
-  brightness: Brightness.light,
-  contrastLevel: 0.2,
-);
-
-var kDarkColorScheme = ColorScheme.fromSeed(
-  brightness: Brightness.dark,
-  seedColor: const Color.fromARGB(255, 1, 12, 20),
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
 );
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  // ]).then(() {
-  //   // then grab runApp() in here
-  // });
-  runApp(
-    MaterialApp(
-        darkTheme: ThemeData.dark().copyWith(
-          colorScheme: kDarkColorScheme,
-          cardTheme: CardTheme().copyWith(
-              color: kDarkColorScheme.secondaryContainer,
-              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: kDarkColorScheme.primaryContainer,
-                foregroundColor: kDarkColorScheme.onPrimaryContainer),
-          ),
-        ),
-        // add this line of material 2 is on
-        theme: ThemeData().copyWith(
-          // scaffoldBackgroundColor: kColorScheme,
-          colorScheme: kColorScheme,
-          cardTheme: CardTheme().copyWith(
-              color: kColorScheme.secondaryContainer,
-              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
-          appBarTheme: AppBarTheme().copyWith(
-              backgroundColor: kColorScheme.onPrimaryContainer,
-              foregroundColor: kColorScheme.primaryContainer),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: kColorScheme.primaryContainer),
-          ),
-          textTheme: ThemeData().textTheme.copyWith(
-                titleLarge: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: kColorScheme.onSecondaryContainer,
-                    fontSize: 18),
-              ),
-        ),
-        // themeMode: ThemeMode.system, already default
-        home: Expenses()),
-  );
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      // home: const CategoriesScreen(), 
+      // test meal screen
+      home: const MealsScreen(
+        title: "Some dummy category...",
+        meals: dummyMeals,
+      ),
+    );
+  }
 }
